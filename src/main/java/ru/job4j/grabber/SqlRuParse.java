05140ec29417ffe209517f7ebdc45103a619b738
Mr. Anderson <h-blast@list.ru>
2020-11-24 22:47:48 +0100

    2. Parsing HTML page. [#318367]

---
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -0,0 +1,22 @@
+package ru.job4j.grabber;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        Elements dates = doc.select(".altCol");
+        int i = 1;
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            System.out.println(dates.get(i).text());
+            i += 2;
+        }
+    }
+}
