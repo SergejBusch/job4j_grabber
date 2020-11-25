07334fc2f4aed1cef73358e162616c229e3809c5
Mr. Anderson <h-blast@list.ru>
2020-11-25 21:00:08 +0100

    2.1.1. Parsing https://www.sql.ru/forum/job-offers/3 [#318371]

--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -14,19 +14,24 @@ import java.util.HashMap;
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         var sqlRuParse = new SqlRuParse();
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements dates = doc.select(".altCol");
-        int i = 1;
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(dates.get(i).text());
-            System.out.println(Date.valueOf(sqlRuParse
-                    .convertDate(dates.get(i).text())));
-            i += 2;
+        for (int siteNumber = 1; siteNumber < 6; siteNumber++) {
+            String adress = "https://www.sql.ru/forum/job-offers";
+            adress = siteNumber > 1 ? adress + "/" + siteNumber : adress;
+            Document doc = Jsoup.connect(adress).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements dates = doc.select(".altCol");
+            int i = 1;
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(dates.get(i).text());
+                System.out.println(Date.valueOf(sqlRuParse
+                        .convertDate(dates.get(i).text())));
+                i += 2;
+            }
         }
+
     }
 
     private LocalDate convertDate(String date) {
