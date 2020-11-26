60ea1adfa231bef2dfdfe68ea283ad6e364614c8
Mr. Anderson <h-blast@list.ru>
2020-11-26 04:21:30 +0100

    2.2 Data model - Post. [#318372]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,10 @@
+package ru.job4j.grabber;
+
+import java.util.Date;
+
+public class Post {
+    private String url;
+    private String title;
+    private Date date;
+    private String detail;
+}
